9c76ce8478e37511a5ccb4b56be6ee54ae138eca
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-17 23:30:55 +0530

    fitness management

--- userservice/src/main/java/com/fitness/userservice/services/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/services/UserService.java
@@ -1,7 +1,12 @@
 package com.fitness.userservice.services;
 
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    public UserResponse register(RegisterRequest request) {
+        return null;
+    }
 }
