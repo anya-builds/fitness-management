0354a8a4dce51100aa422dd617f7c7d3dcc9ea33
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-17 22:28:37 +0530

    fitness management

--- userservice/src/main/java/com/fitness/userservice/models/User.java
+++ userservice/src/main/java/com/fitness/userservice/models/User.java
@@ -1,18 +1,22 @@
 package com.fitness.userservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
 @Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String email;
     private String password;
     private String firstName;
     private String lastName;
     private UserRole role = UserRole.USER;
-    private 
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
