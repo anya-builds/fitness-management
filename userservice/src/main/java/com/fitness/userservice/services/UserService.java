1cd7c85cc49037ffb3d4e812e2ee4250f455e623
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-17 23:45:06 +0530

    fitness management

--- userservice/src/main/java/com/fitness/userservice/services/UserService.java
+++ userservice/src/main/java/com/fitness/userservice/services/UserService.java
@@ -1,12 +1,31 @@
 package com.fitness.userservice.services;
 
+import com.fitness.userservice.UserRepository;
 import com.fitness.userservice.dto.RegisterRequest;
 import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.models.User;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserService {
+    private final UserRepository repository;
     public UserResponse register(RegisterRequest request) {
-        return null;
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setPassword(request.getPassword());
+
+        User savedUser = new User();
+        UserResponse userResponse = new UserResponse();
+        userResponse.setId(savedUser.getId());
+        userResponse.setPassword(savedUser.getPassword());
+        userResponse.setEmail(savedUser.getEmail());
+        userResponse.setFirstName(savedUser.getFirstName());
+        userResponse.setLastName(savedUser.getLastName());
+        userResponse.setCreatedAt(savedUser.getCreatedAt());
+        userResponse.setUpdatedAt(savedUser.getUpdatedAt());
     }
 }
