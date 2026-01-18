516d8a8f54998be1da7d7ab4bf42602e338ac6ed
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-18 21:32:24 +0530

    fitness management

--- activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -1,4 +1,20 @@
 package com.fitness.activityservice.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
+
+@Document()
 public class Activity {
+    private String id;
+    private String userId;
+    private ActivityType type;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private Map<String, Object> additionalMetrics;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
