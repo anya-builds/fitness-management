4b971ed5230cd24acdd4774e818b91fed15f21b2
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-19 00:07:29 +0530

    fitness management

--- activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
+++ activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -1,12 +1,20 @@
 package com.fitness.activityservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Objects;
 
-@Document()
+@Document(collation = "activities")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Activity {
     private String id;
     private String userId;
