ebb467a64d5cb94177647e59eaf17e84e3abdbf9
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-20 14:36:40 +0530

    Fitness management

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -1,7 +1,16 @@
 package com.fitness.activityservice.service;
 
+import com.fitness.activityservice.ActivityRepository;
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+    public ActivityResponse trackActivity(ActivityRequest request) {
+    }
 }
