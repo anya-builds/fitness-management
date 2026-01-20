966f9eba153f939f396c807a67c4f09d287be55e
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-01-20 14:29:25 +0530

    Fitness management

--- activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -1,6 +1,8 @@
 package com.fitness.activityservice.controller;
 
 
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.service.ActivityService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
