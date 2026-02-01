3abd3deff40c430807ce6a7347d5169953f4f6c7
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-02-01 23:31:59 +0530

    fitness

--- eureka/src/main/java/com/fitness/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/fitness/eureka/EurekaApplication.java
@@ -1,15 +1,15 @@
-//package com.fitness.eureka;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-//
-//@SpringBootApplication
-//@EnableEurekaServer
-//public class EurekaApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(EurekaApplication.class, args);
-//	}
-//
-//}
+package com.fitness.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
