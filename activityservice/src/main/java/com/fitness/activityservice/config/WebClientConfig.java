64766fc17f7441a661a3a268d399107c0e71c7b2
Ankita  Arya <113823362+anya-builds@users.noreply.github.com>
2026-02-09 16:54:03 +0530

    fitness

--- activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
@@ -1,7 +1,9 @@
 package com.fitness.activityservice.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+    public WebClient
 }
